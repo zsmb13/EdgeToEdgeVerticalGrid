5e4226151aecbd8941f65912a8f9c868aa5d926b
Márton Braun <marton.braun@jetbrains.com>
2024-09-06 13:42:20 +0200

    Clean up code

--- app/src/main/java/com/example/edgetoedgeverticalgrid/MainActivity.kt
+++ app/src/main/java/com/example/edgetoedgeverticalgrid/MainActivity.kt
@@ -12,41 +12,33 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.edgetoedgeverticalgrid.ui.theme.EdgeToEdgeVerticalGridTheme
 
 class MainActivity : ComponentActivity() {
+    val list = List(50) { it }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
-                val list = List(50) { it }
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(180.dp),
-                    modifier = Modifier.fillMaxSize(),
-                    contentPadding = WindowInsets.safeDrawing.asPaddingValues(),
-                ) {
-                    items(list, key = { it }) { obj ->
-                        Box(
-                            Modifier
-                                .padding(8.dp)
-                                .fillMaxSize()
-                                .aspectRatio(1f)
-                                .background(Color.Blue)
-                        )
-                    }
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(180.dp),
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = WindowInsets.safeDrawing.asPaddingValues(),
+            ) {
+                items(list) {
+                    Box(
+                        Modifier
+                            .padding(8.dp)
+                            .fillMaxSize()
+                            .aspectRatio(1f)
+                            .background(Color.Blue)
+                    )
                 }
             }
         }
